89ff1246106f4fee00c4c41f92084e238bc3a274
ShaqeelAhmad <shaqeelahmad2004@gmail.com>
2021-06-13 18:27:24 +0800

    Added run() function

--- src/fuyunix.c
+++ src/fuyunix.c
@@ -3,6 +3,7 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
+#include "keys.h"
 #include "fuyunix.h"
 #include "drw.h"
 
@@ -11,17 +12,37 @@
 /* Function declarations */
 
 /* Function definitions */
+void 
+run(void)
+{
+	SDL_Event event;
+	bool quit = false;
+
+	while (!quit) {
+		while (SDL_PollEvent(&event) != 0) {
+			if (event.type == SDL_QUIT)
+				quit = true;
+			else if (event.type == SDL_KEYDOWN) {
+				quit = handleKeys(&event.key);
+			}
+			drw();
+		}
+	}
+
+}
+
 int
 main(int argc, char *argv[])
 {
-	int i;
-	if (argc > 1) {
-		for (i = 1; i < argc; i++) {
-			if (strcmp(argv[i], "-v") == 0) {
-				puts(VERSION);
-				return EXIT_SUCCESS;
-			}
+	int i = 0;
+	while (argc > 1 && i < argc) {
+		if (strcmp(argv[i], "-v") == 0) {
+			puts(VERSION);
+			return EXIT_SUCCESS;
 		}
+		i++;
+	}
+	if (argc > 1) {
 		fputs("Usage: fuyunix [-v]\n"
 				"Run fuyunix without any arguments to start the game\n",
 				stderr);
@@ -30,7 +51,7 @@ main(int argc, char *argv[])
 
 	init();
 
-	/* run(); */
+	run();
 
 	cleanup();
 
