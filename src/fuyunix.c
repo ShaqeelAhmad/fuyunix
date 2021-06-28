dc4028fb87052c57bd5bda88b17452d5ff409ed0
ShaqeelAhmad <shaqeelahmad2004@gmail.com>
2021-06-28 16:56:29 +0800

    File handling moved to another file

--- src/fuyunix.c
+++ src/fuyunix.c
@@ -1,18 +1,18 @@
 /*
  *  Copyright 2021 Shaqeel Ahmad
- *  
+ *
  *  This file is part of fuyunix.
- *  
+ *
  *  fuyunix is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  fuyunix is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with fuyunix.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -29,108 +29,12 @@
 #include "drw.h"
 #include "keys.h"
 #include "fuyunix.h"
+#include "file.h"
 
 /* global variables */
 static bool quit = false;
-static int level = 0;
 
 /* Function definitions */
-char *
-getSavePath(void)
-{
-	char *path;
-	char *str;
-	char *dir = "/fuyunix";
-	char *file = "/save";
-
-	path = getenv("XDG_DATA_HOME");
-	if (path == NULL) {
-		path = getenv("HOME");
-		if (path == NULL)
-			fprintf(stderr, "HOME is not set\n");
-
-		dir = "/.fuyunix";
-	}
-
-	char *dirpath;
-	size_t size = strlen(path) + strlen(dir);
-
-	dirpath = malloc(size);
-	snprintf(dirpath, size + 1, "%s%s", path, dir);
-
-	if (mkdir(dirpath, 0755) != 0)
-		if(errno != EEXIST)
-			perror("Unable to create save directory\n");
-
-	size = size + strlen(file);
-	str = malloc(size);
-
-	snprintf(str, size + 1, "%s%s", dirpath, file);
-
-	free(dirpath);
-
-	return str;
-}
-
-void
-handleComments(FILE *fp)
-{
-	while (fgetc(fp) == '#') {
-		while (fgetc(fp) != '\n');
-	}
-}
-
-void
-readSaveFile(void)
-{
-	char *savepath;
-	FILE *fp;
-
-	savepath  = getSavePath();
-
-	fp = fopen(savepath, "r");
-
-	if (fp == NULL) {
-		free(savepath);
-		return;
-	}
-
-	handleComments(fp);
-
-	fscanf(fp, "%*s%d", &level);
-
-	fclose(fp);
-
-	free(savepath);
-}
-
-void
-writeSaveFile(void)
-{
-	char *savepath;
-	FILE *fp;
-
-	savepath  = getSavePath();
-
-	fp = fopen(savepath, "w+");
-	if (fp == NULL) {
-		perror("Couldn't open savefile\n");
-		free(savepath);
-		return;
-	}
-
-	fprintf(fp, "# This file should not be edited by the user\n"
-			"# Do not change the order of the variables"
-			"(Only the values are parsed)\n"
-			"# Comments only work on the top of file\n");
-
-	fprintf(fp, "Level %d\n", level);
-
-	fclose(fp);
-
-	free(savepath);
-}
-
 /* Other parts of program will call quitloop to exit the game */
 void
 quitloop(int player)
@@ -141,7 +45,7 @@ quitloop(int player)
 void
 run(void)
 {
-	readSaveFile();
+	/* readSaveFile(); */
 
 	SDL_Event event;
 
@@ -155,7 +59,7 @@ run(void)
 		drw();
 	}
 
-	writeSaveFile();
+	/* writeSaveFile(); */
 }
 
 int
