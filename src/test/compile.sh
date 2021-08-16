5005db7f93804221e74794537eb08421ba8b24d2
ShaqeelAhmad <shaqeelahmad2004@gmail.com>
2021-08-16 15:05:09 +0800

    Uses multiple threads (with nproc)

--- src/test/compile.sh
+++ src/test/compile.sh
@@ -2,9 +2,12 @@
 
 ## Run from src directory
 
-make CC=tcc && make clean && echo "make tcc success" > ./test/compile.log || echo "make tcc failed" > ./test/compile.log
-make CC=gcc && make clean && echo "make gcc success" >> ./test/compile.log || echo "make gcc failed" >> ./test/compile.log
-make CC=clang && make clean && echo "make clang success" >> ./test/compile.log || echo "make clang failed" >> ./test/compile.log
-bmake CC=tcc && bmake clean && echo "bmake tcc success" >> ./test/compile.log || echo "bmake tcc failed" >> ./test/compile.log
-bmake CC=gcc && bmake clean && echo "bmake gcc success" >> ./test/compile.log || echo "bmake gcc failed" >> ./test/compile.log
-bmake CC=clang && bmake clean && echo "bmake clang success" >> ./test/compile.log || echo "bmake clang failed" >> ./test/compile.log
+threads=$(nproc)
+threads=${threads:-1}
+
+make  "-j${threads}" CC=tcc && make clean && echo "make tcc success" > ./test/compile.log || echo "make tcc failed" > ./test/compile.log
+make  "-j${threads}" CC=gcc && make clean && echo "make gcc success" >> ./test/compile.log || echo "make gcc failed" >> ./test/compile.log
+make  "-j${threads}" CC=clang && make clean && echo "make clang success" >> ./test/compile.log || echo "make clang failed" >> ./test/compile.log
+bmake "-j${threads}" CC=tcc && bmake clean && echo "bmake tcc success" >> ./test/compile.log || echo "bmake tcc failed" >> ./test/compile.log
+bmake "-j${threads}" CC=gcc && bmake clean && echo "bmake gcc success" >> ./test/compile.log || echo "bmake gcc failed" >> ./test/compile.log
+bmake "-j${threads}" CC=clang && bmake clean && echo "bmake clang success" >> ./test/compile.log || echo "bmake clang failed" >> ./test/compile.log
