d35a1e94e2019150241000f5aaf36fac0f229d70
Shaqeel Ahmad <shaqeelahmad2004@gmail.com>
2022-02-02 14:55:07 +0800

    Add debug flag `-g` by default

--- config.mk
+++ config.mk
@@ -7,13 +7,13 @@ RESOURCE_PATH = $(DATADIR)
 
 CC = cc
 
-CFLAGS != pkg-config --cflags cairo sdl2
-
-CFLAGS += -Wall -Werror -pedantic -Wextra -std=c99 -Wno-unused-parameter \
+CFLAGS = -g -Wall -Werror -pedantic -Wextra -std=c99 -Wno-unused-parameter \
 	-DRESOURCE_PATH=\"$(RESOURCE_PATH)\"
+
+CFLAGS += `pkg-config --cflags cairo sdl2`
 
 ## Required for tcc
 CFLAGS += -DSDL_DISABLE_IMMINTRIN_H
 
-LDFLAGS != pkg-config --libs sdl2 cairo SDL2_image
+LDFLAGS = `pkg-config --libs sdl2 cairo SDL2_image`
 LDFLAGS +=  -lm
