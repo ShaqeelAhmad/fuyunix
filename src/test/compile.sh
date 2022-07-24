f0c4779e8c76b30f923a6c052628c5814163de00
Shaqeel Ahmad <04shaqeel@gmail.com>
2022-07-24 19:14:26 +0800

    Simplify test, remove log file

--- src/test/compile.sh
+++ src/test/compile.sh
@@ -2,12 +2,22 @@
 
 ## Run from src directory
 
-threads=$(nproc)
-threads=${threads:-1}
+compile() {
+	Make="$1"
+	CC="$2"
 
-make  "-j${threads}" CC=tcc && make clean && echo "make tcc success" > ./test/compile.log || echo "make tcc failed" > ./test/compile.log
-make  "-j${threads}" CC=gcc && make clean && echo "make gcc success" >> ./test/compile.log || echo "make gcc failed" >> ./test/compile.log
-make  "-j${threads}" CC=clang && make clean && echo "make clang success" >> ./test/compile.log || echo "make clang failed" >> ./test/compile.log
-bmake "-j${threads}" CC=tcc && bmake clean && echo "bmake tcc success" >> ./test/compile.log || echo "bmake tcc failed" >> ./test/compile.log
-bmake "-j${threads}" CC=gcc && bmake clean && echo "bmake gcc success" >> ./test/compile.log || echo "bmake gcc failed" >> ./test/compile.log
-bmake "-j${threads}" CC=clang && bmake clean && echo "bmake clang success" >> ./test/compile.log || echo "bmake clang failed" >> ./test/compile.log
+	"$Make" clean 2>&1 >&-
+	if "$Make" CC="$CC" 2>&1 >&-
+	then
+		printf "%s %s success\n" "$Make" "$CC"
+	else
+		printf "%s %s failed\n" "$Make" "$CC"
+	fi
+}
+
+compile make tcc
+compile make gcc
+compile make clang
+compile bmake tcc
+compile bmake gcc
+compile bmake clang
